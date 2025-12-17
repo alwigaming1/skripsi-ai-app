3898daf2b2c188217575afffd7b13ee2da9f5371
Alwi <alwigaming1@gmail.com>
2025-12-18 06:01:21 +0800

    update vercel and index

--- public/index.php
+++ public/index.php
@@ -1,39 +1,17 @@
 <?php
 
-// 1. Load Composer Autoloader
-require __DIR__ . '/../vendor/autoload.php';
+use Illuminate\Http\Request;
 
-// 2. Load Aplikasi Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+define('LARAVEL_START', microtime(true));
 
-// 3. --- FIX VERCEL READ-ONLY FILESYSTEM ---
-// Kita paksa Laravel menggunakan folder /tmp untuk penyimpanan sementara
-$storagePath = '/tmp/storage';
+require __DIR__.'/../vendor/autoload.php';
 
-if (!is_dir($storagePath)) {
-    mkdir($storagePath, 0777, true);
-    mkdir($storagePath . '/framework/views', 0777, true);
-    mkdir($storagePath . '/framework/cache', 0777, true);
-    mkdir($storagePath . '/framework/sessions', 0777, true);
-    mkdir($storagePath . '/logs', 0777, true);
-}
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Set path baru ke aplikasi
-$app->useStoragePath($storagePath);
-
-// Paksa Config Cache ke /tmp agar tidak bentrok dengan file sisa Windows
-$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-
-// 4. Jalankan Aplikasi
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
+    $request = Request::capture()
+)->send();
 
 $kernel->terminate($request, $response);
